7021abed1b4e7dab2213ec72e89b05e94ca0c171
saulo775 <59702227+saulo775@users.noreply.github.com>
2021-02-12 16:18:10 -0300

    TESTE - It should not be able to create two appointments on the same time

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '..//repositories/fakes/FakeAppointmentsRepository'
 
 import CreateAppointmentService from './CreateAppointmentService';
@@ -16,7 +18,22 @@ describe('CreateAppointment', () => {
         expect(appointment.provider_id).toBe('123123');
    });
 
-//    it('should not be able to create two appointments on the same time', () => {
+   it('should not be able to create two appointments on the same time',async() => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
 
-//    })
+        const appointmentDate = new Date(2020, 4, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123123',
+        });
+
+        expect(createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '123123',
+        })).rejects.toBeInstanceOf(AppError)
+   })
 });
