592c8ddf80d5f9ae09399eafa3c96b82a94fd1af
saulo775 <59702227+saulo775@users.noreply.github.com>
2021-02-13 15:12:08 -0300

    TDD - Teste de envio de email

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,7 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider,
