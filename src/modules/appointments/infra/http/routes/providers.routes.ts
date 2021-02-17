5f8ec0722b401e99746ee600aaff2c22c53386f4
saulo775 <59702227+saulo775@users.noreply.github.com>
2021-02-16 23:49:46 -0300

    Rotas e controllers para agendamento de serviços

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,19 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderDayAvailability from '../controllers/ProviderDayAvailability';
+import ProviderMonthAvailability from '../controllers/ProviderMonthAvailability';
+
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerDayAvailability = new ProviderDayAvailability()
+const providerMonthAvailability = new ProviderMonthAvailability()
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get('/:provider_id/month-availability', providerMonthAvailability.index);
+providersRouter.get('/:provider_id/day-availability', providerDayAvailability.index);
 
 export default providersRouter;
