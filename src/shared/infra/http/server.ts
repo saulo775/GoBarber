a74a215e7f19d653ce0890d62ba60bfbe2ef8ebb
saulo775 <59702227+saulo775@users.noreply.github.com>
2021-02-18 20:37:12 -0300

    Express rate limit - Lidando com ataques de brute force

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,13 +8,17 @@ import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewars/rateLimiter';
 import routes from './routes';
+
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
 
+
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
