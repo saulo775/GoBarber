d1648000e5febc938fc65e29dc5af447244b7f94
saulo775 <59702227+saulo775@users.noreply.github.com>
2021-02-18 10:44:09 -0300

    Organizando o container

---
+++ src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,15 @@
+import { container } from 'tsyringe';
+
+import IMailTemplateProvider from './models/IMailTemplateProvider';
+
+import HandlebarsMailTemplateProvider from './implementations/HandlebarsMailTemplateProvider';
+
+const providers =  {
+    handlebars: HandlebarsMailTemplateProvider,
+}
+
+
+container.registerSingleton<IMailTemplateProvider>(
+    'MailTemplateProvider',
+    providers.handlebars,
+);
