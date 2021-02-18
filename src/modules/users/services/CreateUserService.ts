52bf1595bd82e15656a1c7de870d48d5f33d60c3
saulo775 <59702227+saulo775@users.noreply.github.com>
2021-02-18 16:29:59 -0300

    Invalidando cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe'
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/implemantations/BCryptHashProvider';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import User from '../infra/typeorm/entities/User';
 
@@ -20,7 +21,10 @@ class CreateUserService {
         private usersRepository: IUsersRepository,
 
         @inject('HashProvider')
-        private hashProvider: IHashProvider
+        private hashProvider: IHashProvider,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {};
 
     public async execute({name, email, password }: IRequest): Promise<User>{
@@ -39,6 +43,7 @@ class CreateUserService {
            password: hashedPassword,
        });
 
+       await this.cacheProvider.invalidatePrefix('providers-list')
 
        return user;
 
